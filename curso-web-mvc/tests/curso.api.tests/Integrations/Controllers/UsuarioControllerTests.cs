5114cafb0777c3ba84bda41416dde69525ac94e0
Roberto Carlos <betocarlos00@hotmail.com>
2022-03-28 22:52:48 -0300

    test user login

---
+++ curso-web-mvc/tests/curso.api.tests/Integrations/Controllers/UsuarioControllerTests.cs
@@ -0,0 +1,68 @@
+﻿using curso.api.Models.Usuarios;
+using curso.web.mvc;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace curso.api.tests.Integrations.Controllers
+{
+    public class UsuarioControllerTests : IClassFixture<WebApplicationFactory<Startup>>, IAsyncLifetime
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+        private readonly HttpClient _httpClient;
+
+        public UsuarioControllerTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+            _httpClient = _factory.CreateClient();
+        }
+
+        public Task DisposeAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task InitializeAsync()
+        {
+            //Arrange
+            var loginViewModelInput = new LoginViewModelInput
+            {
+                Login = "user",
+                Senha = "123456"
+            };
+
+            StringContent content = new StringContent(JsonConvert.SerializeObject(loginViewModelInput), Encoding.UTF8, "application/json");
+
+            //Action
+            var httpClient = await _httpClient.PostAsync("api/v1/usuario/logar", content);
+
+            //Assert
+            Assert.Equal(System.Net.HttpStatusCode.OK, httpClient.StatusCode);
+        }
+
+        [Fact]
+        public async Task Logar_informandoUsuarioESenhaExistente_RetornaOk()
+        {
+            //Arrange
+            var loginViewModelInput = new LoginViewModelInput
+            {
+                Login = "user",
+                Senha = "123456"
+            };
+
+            StringContent content = new StringContent(JsonConvert.SerializeObject(loginViewModelInput), Encoding.UTF8, "application/json");
+
+            //Action
+            var httpClient = await _httpClient.PostAsync("api/v1/usuario/logar", content);
+
+            //Assert
+            Assert.Equal(System.Net.HttpStatusCode.OK, httpClient.StatusCode);
+        }
+    }
+}
